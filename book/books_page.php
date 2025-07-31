bd353c1f556ca12cad4cdd8f8124ca785cb0facd
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-07-31 20:09:10 +0530

    update navbar

--- book/books_page.php
+++ book/books_page.php
@@ -5,12 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
   <link rel="stylesheet" href="styles.css">
   <link rel="stylesheet" href="media.css">
 </head>
 <body>
   <!-- navbar -->
-  <header class="container-fluid bg-primary m-0 p-0" style="height: 70px;">
+  <header class="container-fluid  m-0 p-0" style="height: 70px; background-color:#524E4E;">
     <div class="container ">
       <div class="row  m-0 mb-2 p-0">
         <div class="col-12">
@@ -19,25 +20,25 @@
             <button style="margin-top: -20px;" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse justify-content-end mx-4" id="navbarSupportedContent">
               <ul class="navbar-nav" style="margin-top: -20px;">
                 <li class="nav-item">
-                  <a class="nav-link" href="#"><span style="color: black;">Home</span></a>
+                  <a class="nav-link" href="#"><span style="color: white;">Home</span></a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#"><span style="color: black;">History</span></a>
+                  <a class="nav-link" href="#"><span style="color: white;">History</span></a>
                 </li>
                 <li class="nav-item">
                 <!-- Search -->
-                <form class="d-flex" role="search">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                  <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                <div class="d-flex mb-1"   role="search">
+                  <input class="form-control me-2" type="search" style="padding-left:50px; border-radius: 50px; width: 400px; background-color:#524E4E; border:2px solid white;" placeholder="Search ..." aria-label="Search"/>
+                  <i class="fa-solid fa-magnifying-glass " style="margin-top:10px; width:50px;color:#ffffff;  position: absolute; "></i>
+                </div>
                 <!-- Search -->
                 </li>
                 <li class="nav-item">
-                  <input class="btn btn-secondary" style="background-color: #8F8484; color: white;" type="button" value="Generate history pdf">
-                  <input class="btn btn-secondary " style="background-color: #FF000D; color: white;" type="button" value="LogOut">
+                  <input class="btn btn-secondary" style="background-color: #8F8484; color: white; border:2px solid white;" type="button" value="Generate history pdf">
+                  <input class="btn btn-secondary " style="background-color: #FF000D; color: white; border:2px solid white;" type="button" value="LogOut">
                 </li>
               </ul>
             </div>
