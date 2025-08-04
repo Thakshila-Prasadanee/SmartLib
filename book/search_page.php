8a3c143005b2f5d24b93d4ea03d107fd192d8325
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-08-04 22:07:28 +0530

    update search_page.php

--- book/search_page.php
+++ book/search_page.php
@@ -61,7 +61,11 @@
                 <!-- Search -->
                 </li>
                 <li class="nav-item">
-                  <input class="btn btn-secondary" style="background-color: #8F8484; color: white; border:2px solid white;" type="button" value="Generate history pdf">
+                  <input class="btn btn-secondary" 
+                    style="background-color: #8F8484; color: white; border:2px solid white;" 
+                    type="button" 
+                    value="Generate history pdf" 
+                    onclick="window.open('generate_history_pdf.php', '_blank')">
                   <input class="btn btn-secondary " style="background-color: #FF000D; color: white; border:2px solid white;" type="button" value="LogOut">
                 </li>
               </ul>
